295c7b8533e7e1e056225fe9fe23466d4813a5d7
Fazteel <100552801+Fazteel@users.noreply.github.com>
2024-09-11 12:01:40 +0700

    'Update'

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const fs = require("fs");  // Import modul fs
+const fs = require("fs");
+const validator = require("validator");
 
-fs.readFile("test.txt", "utf-8", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-});
+// fs.readFile("test.txt", "utf-8", (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+const email = 'bangtantogether@gmail.com';
+
+if (validator.isEmail(email)) {
+    console.log("Format email anda benar");
+} else {
+    console.log("Format email anda salah");
+}
--- readline.js
+++ readline.js
@@ -1,21 +1,35 @@
 const readline = require("node:readline");
 const fs = require("fs");
+const validator = require("validator");
 const { log } = require("node:console");
 
 const { stdin: input, stdout: output } = require("node:process");
+const { default: isEmail } = require("validator/lib/isEmail");
 
 const rl = readline.createInterface({ input, output });
 
 rl.question("Nama: ", (name) => {
     rl.question("No Hp: ", (mobile) => {
         rl.question("Email: ", (email) => {
-            const result = [ {
-                name,
-                mobile,
-                email
-            }];
-            fs.writeFileSync("text.txt", JSON.stringify(result));
-            console.log("Data mu tersimpan di file");
+            const validMobile = validator.isMobilePhone(mobile);
+            const validEmail = validator.isEmail(email);
+
+            if (validMobile && validEmail) {
+                const result = [ {
+                    name,
+                    mobile,
+                    email
+                } ];
+                fs.writeFileSync("text.txt", JSON.stringify(result));
+                console.log("Data mu tersimpan di file");
+            } else {
+                if (!validEmail) {
+                    console.log("Email anda salah");
+                }
+                if (!validMobile) {
+                    console.log("No Telepon anda salah");
+                }
+            }
             rl.close();
         })
     });
