9ec0c58eb75425dc372989c14dea5e5521319c56
Fazteel <100552801+Fazteel@users.noreply.github.com>
2024-09-12 11:52:36 +0700

    'Update'

--- function.js
+++ function.js
@@ -1,8 +1,50 @@
-const fs = require("fs");  // Import modul fs
+const fs = require("fs");
+const validator = require("validator");
+const { default: isEmail } = require("validator/lib/isEmail");
+const { readFile, readFileSync } = require("node:fs");
 
 function generate(name, mobile) {
     console.log("Nama saya", name + ', ' + "Nomer Telp saya", mobile);
 }
 
-// Gunakan fs.writeFileSync untuk menulis ke file index.txt
 fs.writeFileSync('index.txt', 'Saya Fahmi test');
+
+const addData = (name, mobile, email) => {
+    const validEmail = validator.isEmail(email);
+    const validMobile = validator.isMobilePhone(mobile);
+
+    if (validMobile && validEmail) {
+        return {
+            name,
+            mobile,
+            email
+        };
+    } else {
+        if (!validEmail) {
+            console.log("Email anda salah");
+        }
+        if (!validMobile) {
+            console.log("No Telepon anda salah");
+        }
+        return null;
+    }
+};
+
+const saveData = (result) => {
+    try {
+        const file = fs.readFileSync("data/contacts.json", "utf-8");
+        const contact = JSON.parse(file);
+        contact.push(result);
+
+        fs.writeFileSync("data/contacts.json", JSON.stringify(contact, null, 2));
+        console.log("Data mu tersimpan di file");
+    } catch (error) {
+        console.log("Terjadi kesalahan", error.message);
+        
+    }
+}
+
+module.export = {
+    addData,
+    saveData
+}
--- readline.js
+++ readline.js
@@ -9,31 +9,48 @@ const { readFile, readFileSync } = require("node:fs");
 
 const rl = readline.createInterface({ input, output });
 
+function addData(name, mobile, email){
+    const validEmail = validator.isEmail(email);
+    const validMobile = validator.isMobilePhone(mobile);
+
+    if (validMobile && validEmail) {
+        return {
+            name,
+            mobile,
+            email
+        };
+    } else {
+        if (!validEmail) {
+            console.log("Email anda salah");
+        }
+        if (!validMobile) {
+            console.log("No Telepon anda salah");
+        }
+        return null;
+    }
+};
+
+function saveData(result){
+    try {
+        const file = fs.readFileSync("data/contacts.json", "utf-8");
+        const contact = JSON.parse(file);
+        contact.push(result);
+
+        fs.writeFileSync("data/contacts.json", JSON.stringify(contact, null, 2));
+        console.log("Data mu tersimpan di file");
+    } catch (error) {
+        console.log("Terjadi kesalahan", error.message);
+        
+    }
+};
+
 rl.question("Nama: ", (name) => {
     rl.question("No Hp: ", (mobile) => {
         rl.question("Email: ", (email) => {
-            const validEmail = validator.isEmail(email);
-            const validMobile = validator.isMobilePhone(mobile);
+            const newContact = addData(name, mobile, email);
 
-            if (validMobile && validEmail) {
-                const result = {
-                    name,
-                    mobile, 
-                    email
-                };
-                const file = fs.readFileSync("data/contacts.json", "utf-8");
-                const contact = JSON.parse(file);
-                contact.push(result);
-
-                fs.writeFileSync("data/contacts.json", JSON.stringify(contact, null, 2));
-                console.log("Data mu tersimpan di file");
-            } else {
-                if (!validEmail) {
-                    console.log("Email anda salah");
-                }
-                if (!validMobile) {
-                    console.log("No Telepon anda salah");
-                }
+            if (newContact) {
+                saveData(newContact)
             }
             rl.close();
         })
